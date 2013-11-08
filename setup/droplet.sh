ee0be8230a902cdc3938658a84db96a3ece41fd9
Holman Gao <holman@golmansax.com>
2013-11-08 00:17:48 -0800

    Fixing droplet script

--- setup/droplet.sh
+++ setup/droplet.sh
@@ -4,21 +4,38 @@
 #
 # Sets up a DigitalOcean droplet for my liking, tested on Ubuntu 13.10
 
-ME=holman
+USER=holman
+ME=`whoami`
 
-# Setup user
-adduser $ME
+if [ $ME = 'root' ]; then
+  echo "*** Let's change the root password... ***"
+  passwd
 
-# Setup visudo
-update-alternatives --config editor
+  echo "*** Time to add myself ***"
+  # Setup user
+  adduser $USER
 
-# Setup sudo privilages
-visudo
+  # Setup visudo
+  update-alternatives --config editor
 
-# Now get github and my other config files
-su $ME
-cd /home/$ME
-mkdir src
-cd src
-sudo apt-get install -y git-core
-git clone https://github.com/golmansax/my-config.git
+  # Setup sudo privilages
+  visudo
+
+  $THIS_SCRIPT = `basename $0`
+  echo "*** Now run the following commands ***"
+  echo "chown $USER:$USER $THIS_SCRIPT"
+  echo "mv $THIS_SCRIPT /home/$USER/"
+  echo "su $USER"
+  echo "./$THIS_SCRIPT"
+else
+  # Now get github and my other config files
+  echo "*** Installing git and config files ***"
+  mkdir -p src
+  cd src
+  sudo apt-get install -y git-core
+  git clone https://github.com/golmansax/my-config.git
+
+  echo "*** Basic setup installed ***"
+  echo "*** You can now remove this file ***"
+  echo "rm $THIS_SCRIPT"
+fi
