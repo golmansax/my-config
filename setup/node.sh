c9b5c983b6dcaf0e21c112205998aff7ccd44b6f
Holman Gao <holman@golmansax.com>
2013-12-25 10:31:32 -0800

    Adding line to node script

--- setup/node.sh
+++ setup/node.sh
@@ -8,5 +8,8 @@
 sudo apt-get update
 sudo apt-get install -y nodejs npm
 
+# Symlink nodejs binary to node (for node scripts)
+ln -s /usr/bin/nodejs /usr/bin/node
+
 # Install RequireJS optimizer
 sudo npm install -g requirejs
