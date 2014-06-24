744076901e95312f34d0090f3a10b9f4607ce23d
Holman Gao <holman@golmansax.com>
2014-06-24 11:51:51 -0700

    Updating setup script more

--- setup/machine/laptop.sh
+++ setup/machine/laptop.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 # @author holman
 #
-# Sets up a laptop (or another real machine)
+# Sets up a laptop (or another real machine) with Xubuntu 14.04
 #
 # NOTE: This assumes that my-config Github has been downloaded already
 
 sudo apt-get update
 sudo apt-get purge -y pidgin
-sudo apt-get install -y chromium-browser vim redshift
+sudo apt-get install -y chromium-browser vim redshift pepperflashplugin-nonfree
+sudo update-pepperflashplugin-nonfree --install
 sudo apt-get autoremove -y
 sudo apt-get upgrade -y
 
