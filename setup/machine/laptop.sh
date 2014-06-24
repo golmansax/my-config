feed35a4143dce86e6e808c524d69a5af2598a84
Holman Gao <holman@golmansax.com>
2014-06-24 10:59:49 -0700

    Updating laptop script

--- setup/machine/laptop.sh
+++ setup/machine/laptop.sh
@@ -5,12 +5,13 @@
 #
 # NOTE: This assumes that my-config Github has been downloaded already
 
-sudo update-alternatives --config editor
 sudo apt-get update
 sudo apt-get purge -y pidgin
 sudo apt-get install -y chromium-browser vim redshift
 sudo apt-get autoremove -y
 sudo apt-get upgrade -y
+
+sudo update-alternatives --config editor
 
 # Personal setup scripts
 /home/holman/src/my-config/setup/machine/user_dirs.sh
